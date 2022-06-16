25c31e72f5939a635468f44e499c20193bcc0627
Akshay Pisal <akshay.pisal@codeb.online>
2022-06-16 16:01:15 +0530

    feat : changed components name

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 // import { useSelector } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ActiveCoinStrategy from './screens/postlogin/Dashboard/ActiveCoinStrategy';
+import AlgoTradingDashboard from './screens/postlogin/Dashboard/ActiveCoinStrategy';
+// import ActiveCoinStrategy from './screens/postlogin/Dashboard/ActiveCoinStrategy';
 
 function App() {
   return (
     <div className="flex flex-col bg-theme-v2-white2 min-h-screen sticky top-0 z-50 text-[16px]">
       <Router>
         <Routes>
-          <Route path="/" element={<ActiveCoinStrategy />} />
+          <Route path="/" element={<AlgoTradingDashboard />} />
         </Routes>
       </Router>
     </div>
